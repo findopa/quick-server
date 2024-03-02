3bca5f37e1f68e83e871a345d7755f81397420f7
findopa <161636262+findopa@users.noreply.github.com>
2024-03-03 01:06:25 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+    if (req.url == '/now') {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ now: new Date() }));
+        res.end();
+
+    } else {
+
+        res.end('Invalid request');
+    }
+});
+
+server.listen(8080);
+
+console.log('server running on port 8080');
